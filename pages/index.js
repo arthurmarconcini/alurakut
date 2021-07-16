2f360eca19e02f7d224b6485bd7ddaa1354ae8d5
Arthur Marconcini <arthur.m.ramos@hotmail.com>
2021-07-16 16:47:20 -0300

    aula 5 - auth*

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
@@ -51,9 +53,9 @@ function ProfileRelationsBox(props) {
   )
 }
 
-export default function Home() {
+export default function Home(props) {
   const [comunidades, setComunidades] = useState([])
-  const githubUser = 'arthurmarconcini'
+  const githubUser = props.githubUser
   const pessoasFavoritas = [
     'omariosouto',
     'juunegreiros',
@@ -214,4 +216,32 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN 
+
+  const {isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  }).then((resposta) => resposta.json())  
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token)  
+
+  return {
+    props : {
+      githubUser
+    },
+  }
 }
